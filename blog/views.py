29eefbc8d7c8c406e8b8697cdf2166b084e63439
DrNope <pro.sabd@gmail.com>
2024-08-27 10:06:40 +0200

    feat: :rocket: more stuff -> edit post is author or superuser (security) + edit page (crud step 2)

--- blog/views.py
+++ blog/views.py
@@ -20,9 +20,27 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
+            post.created_date = timezone.now()
             post.published_date = timezone.now()
             post.save()
             return redirect('/')
     else:
         form = PostForm()
     return render(request, 'blog/post_new.html', {'form': form})
+
+def post_edit(request, pk):
+    try:
+        post = get_object_or_404(Post, pk=pk)
+        if request.method == "POST":
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('/')
+        else:
+            form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form})
+    except Http404:
+        return render(request, 'blog/post_edit.html', {'form': None}, status=404)
