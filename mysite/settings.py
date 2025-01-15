cbde7ec0dc91eb0b131691c96371e5611e107442
prosabd <102315258+prosabd@users.noreply.github.com>
2025-01-15 22:34:39 +0100

    feat: :construction: mysql database method (wip)

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('db_name'),
+        'USER': os.getenv('db_user'),
+        'PASSWORD': os.getenv('db_password'),
+        'HOST': os.getenv('db_host', 'localhost'),
+        'PORT': os.getenv('db_port', '3306'),
     }
 }
 # Password validation
